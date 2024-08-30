f60123dc7c8bc7d2299a46bd010510108b7cb72c
Simon Willison <swillison@gmail.com>
2024-08-30 11:04:06 -0700

    Tests for editing and checking a permission (#6)
    Closes #3, refs #4

--- tests/test_acl.py
+++ tests/test_acl.py
@@ -74,3 +74,85 @@ async def test_update_dynamic_groups():
         {"id": 1, "name": "admin"},
         {"id": 2, "name": "static"},
     ]
+
+
+@pytest.mark.asyncio
+async def test_permission_allowed():
+    datasette = Datasette(
+        config={
+            "plugins": {
+                "datasette-acl": {
+                    "dynamic-groups": {
+                        "admin": {"is_admin": True},
+                    }
+                }
+            },
+            "permissions": {"datasette-acl": {"id": "root"}},
+        }
+    )
+    db = datasette.add_memory_database("db")
+    await db.execute_write("create table t (id primary key)")
+    await datasette.invoke_startup()
+    db = datasette.get_internal_database()
+    admin_actor = {"id": "simon", "is_admin": True}
+    # That group should exist
+    group_id = (
+        await db.execute("select id from acl_groups where name = 'admin'")
+    ).single_value()
+    assert group_id == 1
+    allowed = await datasette.permission_allowed(
+        actor=admin_actor, action="insert-row", resource=["db", "t"]
+    )
+    assert not allowed
+    # Use the /db/table/-/acl page to insert a permission
+    csrf_token_response = await datasette.client.get(
+        "/db/t/-/acl",
+        cookies={
+            "ds_actor": datasette.client.actor_cookie({"id": "root"}),
+        },
+    )
+    csrftoken = csrf_token_response.cookies["ds_csrftoken"]
+    response = await datasette.client.post(
+        "/db/t/-/acl",
+        data={
+            "permissions_admin_insert-row": "on",
+            "csrftoken": csrftoken,
+        },
+        cookies={
+            "ds_actor": datasette.client.actor_cookie({"id": "root"}),
+            "ds_csrftoken": csrftoken,
+        },
+    )
+    assert response.status_code == 302
+    acls = [
+        dict(r)
+        for r in (
+            await db.execute(
+                """
+        select
+          acl_groups.name as group_name,
+          acl_actions.name as action_name,
+          acl_resources.database as database_name,
+          acl_resources.resource as resource_name
+        from acl
+        join acl_groups on acl.group_id = acl_groups.id
+        join acl_actions on acl.action_id = acl_actions.id
+        join acl_resources on acl.resource_id = acl_resources.id
+    """
+            )
+        )
+    ]
+    assert acls == [
+        {
+            "group_name": "admin",
+            "action_name": "insert-row",
+            "database_name": "db",
+            "resource_name": "t",
+        }
+    ]
+    allowed = await datasette.permission_allowed(
+        actor=admin_actor,
+        action="insert-row",
+        resource=["db", "t"],
+    )
+    assert allowed

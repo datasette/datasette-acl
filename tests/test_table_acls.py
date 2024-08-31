5496c8462d78e8834c2fe29b9484ee7ccf17e82b
Simon Willison <swillison@gmail.com>
2024-08-31 16:19:51 -0700

    Refactor tests a bit
    To make space for the new group tests in #5

--- tests/test_table_acls.py
+++ tests/test_table_acls.py
@@ -17,34 +17,6 @@ ManageTableTest = namedtuple(
 )
 
 
-@pytest_asyncio.fixture
-async def ds():
-    datasette = Datasette(
-        config={
-            "plugins": {
-                "datasette-acl": {
-                    "dynamic-groups": {
-                        # Users with is_staff: True are in staff group
-                        "staff": {"is_staff": True},
-                    }
-                }
-            },
-            # Root user can edit permissions
-            "permissions": {"datasette-acl": {"id": "root"}},
-        }
-    )
-    db = datasette.add_memory_database("db")
-    await db.execute_write("create table t (id primary key)")
-    await datasette.invoke_startup()
-    yield datasette
-    # Need to manually drop because in-memory databases shared across tests
-    await db.execute_write("drop table t")
-    internal_db = datasette.get_internal_database()
-    for table in await internal_db.table_names():
-        if table.startswith("acl"):
-            await internal_db.execute_write(f"drop table {table}")
-
-
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ManageTableTest._fields,
